30ebb28b0f18160c4e7196a58aac98f126a24026
andrea07-art <lucchetti.andrea@itiscannizzaro.edu.it>
2025-05-16 08:50:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
     async function loadProductData(category) {
       
         const data = {
-            smartphone: [
-                { name: "Smartphone 1", description: "Display 6.5", price: "599", image: "https://via.placeholder.com/150" },
-                { name: "Smartphone 2", description: "Camera 12MP", price: "699", image: "https://via.placeholder.com/150" }
-            ],
+           
             automobile: [
                 { name: "Auto 1", description: "Sedan, 150CV", price: "20000", image: "https://via.placeholder.com/150" },
                 { name: "Auto 2", description: "SUV, 250CV", price: "30000", image: "https://via.placeholder.com/150" }
-            ],
-            computer: [
-                { name: "Laptop 1", description: "16GB RAM, 512GB SSD", price: "1200", image: "https://via.placeholder.com/150" },
-                { name: "Laptop 2", description: "8GB RAM, 256GB SSD", price: "800", image: "https://via.placeholder.com/150" }
             ],
            
         };
